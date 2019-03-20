36feb0a57a41d532279ea0e5d1dbe806db340783
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 07:28:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = { name => "Kim" }
   
 end
 
@@ -9,7 +9,10 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  the_manifest = { whale => 5,
+    porcelain => 2,
+    oil => 3
+  }
 
 end
 
@@ -31,13 +34,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun powder"] = 4 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
